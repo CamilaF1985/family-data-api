20958445902acf7a67c09f0410cbf82fd9a92cf2
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-12-24 14:56:17 -0300

    final

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
+# Inicializar la estructura de datos
 jackson_family = FamilyStructure("Jackson")
 
 # Miembros iniciales de la familia
@@ -48,24 +49,31 @@ def sitemap():
 # Obtener todos los miembros de la familia
 @app.route('/members', methods=['GET'])
 def get_all_members():
-    members = jackson_family.get_all_members()
-    return jsonify(members), 200
+    try:
+        members = jackson_family.get_all_members()
+        if members:
+            return jsonify(members), 200
+        else:
+            return jsonify({"error": "Miembros no encontrados"}), 404
+    except Exception as e:
+        return jsonify({"error": "Solicitud incorrecta"}), 400
 
 # Obtener un miembro específico por id
 @app.route('/member/<int:member_id>', methods=['GET'])
 def get_member(member_id):
-    member = jackson_family.get_member(member_id)
-    if member is not None:
-        # Cambiar "name" por "first_name", y agregar "id", "age", "lucky_numbers"
-        return jsonify({
-            "id": member.get("id"),
-            "first_name": member.get("first_name"),
-            "age": member.get("age"),
-            "lucky_numbers": member.get("lucky_numbers", [])  # Asegúrate de que lucky_numbers exista
-        }), 200
-    else:
-        # Devolver un objeto JSON con el código de estado 404 y un mensaje de error
-        return jsonify({"error": "Member not found", "message": "Member not found"}), 404
+    try:
+        member = jackson_family.get_member(member_id)
+        if member is not None:
+            return jsonify({
+                "id": member.get("id"),
+                "first_name": member.get("first_name"),
+                "age": member.get("age"),
+                "lucky_numbers": member.get("lucky_numbers", [])  
+            }), 200
+        else:
+            return jsonify({"error": "Miembro no encontrado", "message": "Member not found"}), 404
+    except Exception as e:
+        return jsonify({"error": "Solicitud incorrecta"}), 400
 
 # Añadir un nuevo miembro
 @app.route('/member', methods=['POST'])
@@ -73,9 +81,9 @@ def add_member():
     try:
         new_member = request.get_json()
         jackson_family.add_member(new_member)
-        return jsonify({}), 200
+        return jsonify({"done": True}), 200
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": "Solicitud incorrecta"}), 400
 
 # Eliminar un miembro por id
 @app.route('/member/<int:member_id>', methods=['DELETE'])
@@ -85,9 +93,10 @@ def delete_member(member_id):
         if deleted:
             return jsonify({"done": True}), 200
         else:
-            return jsonify({"error": "Member not found", "message": "Member not found"}), 404
+            return jsonify({"error": "Miembro no encontrado"}), 404
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": "Solicitud incorrecta"}), 400
+  
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -2,39 +2,46 @@ from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
+        # Inicializar una estructura de familia con un apellido dado
         self.last_name = last_name
         self._members = []
 
     def _generateId(self):
+        # Generar un ID aleatorio para un miembro de la familia
         return randint(0, 99999999)
 
     def add_member(self, member):
+        # Añadir un nuevo miembro a la familia con un ID generado y devolver ese ID
         member_id = self._generateId()
         member["_id"] = member_id
         self._members.append(member)
         return member_id
 
     def delete_member(self, member_id):
-        for i, member in enumerate(self._members):
-            if member["_id"] == member_id:
-                del self._members[i]
-                return True  # Indicate success
-        return False  # Indicate failure (member not found)
+        # Eliminar un miembro de la familia por su ID y devolver True si se realizó la eliminación
+        self._members
+        initial_length = len(self._members)
+        self._members = [m for m in self._members if m.get("id") != member_id]
+        return len(self._members) < initial_length
 
     def get_member(self, member_id):
+        # Obtener un miembro de la familia por su ID
         member = next((m for m in self._members if m.get("id") == member_id), None)
         return member
 
     def update_member(self, member_id, updated_member):
+        # Actualizar la información de un miembro existente en la familia
         for i, member in enumerate(self._members):
             if member["_id"] == member_id:
                 self._members[i] = updated_member
                 self._members[i]["_id"] = member_id
-                return True  # Indicate success
-        return False  # Indicate failure (member not found)
+                return True  
+        return False  
 
     def get_all_members(self):
+        # Obtener una lista de todos los miembros de la familia
         return self._members
+
 
 
 
